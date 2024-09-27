db35cb38145ace029ad1a73098b47592f9bdc9ae
HenryRukundo3 <145010437+HenryRukundo3@users.noreply.github.com>
2024-09-27 22:57:49 +0200

    feat:added filterbar and dependecies

---
+++ app/amalisov.cuibono/webapp/controller/Participants.controller.ts
@@ -0,0 +1,12 @@
+import Controller from "sap/ui/core/mvc/Controller";
+
+/**
+ * @namespace amalisov.cuibono.controller
+ */
+export default class Participants extends Controller {
+
+    /*eslint-disable @typescript-eslint/no-empty-function*/
+    public onInit(): void {
+
+    }
+}
