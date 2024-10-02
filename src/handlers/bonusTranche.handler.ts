ecf67e54c3866e47eb09bfa5499874a86e70e643
Bateyjosue <josuebatey19@gmail.com>
2024-10-02 15:02:15 +0200

    feat: add logic to create bomus tranche with its targets

--- src/handlers/bonusTranche.handler.ts
+++ src/handlers/bonusTranche.handler.ts
@@ -1,15 +1,41 @@
-import cds from "@sap/cds";
-import { Handler, OnRead } from "cds-routing-handlers";
+import cds, { Request } from "@sap/cds";
+import { BeforeCreate, Handler, Req } from "cds-routing-handlers";
 import { Service } from "typedi";
-import { BonusTranche } from "../../@cds-models/BonusTrancheService";
+import { BonusTranche, Target } from "../../@cds-models/BonusTrancheService";
 
 const logger = cds.log("Bonus Tranche handler.");
 
 @Service()
 @Handler(BonusTranche.name)
 export class BonusTrancheHandler {
-  @OnRead()
-  public async onRead() {
-    logger.info("Bonus Tranche on Read handler!");
+
+  @BeforeCreate()
+  public async beforeCreate(@Req() req: Request) {
+    logger.info("Bonus Tranche on Create handler!");
+
+    const targets: Target[] = req.data.Target
+    const { beginDate, endDate, ID: bonusTrancheId } = req.data
+    const currentDate = new Date();
+    const formattedBeginDate = new Date(beginDate);
+    const formattedEndDate = new Date(endDate);
+    
+    if (!(formattedBeginDate > currentDate)) {
+      return req.error(400, "Begin Date must be in the future");
+    }
+
+    if (!(formattedEndDate > formattedBeginDate)) {
+      return req.error(400, "End Date must be after begin date");
+    }
+
+    console.log(targets)
+
+    if (!targets) {
+      return 0;
+    }
+
+    for (const target of targets) {
+      target.BonusTranche_ID = bonusTrancheId
+      await INSERT.into(Target).entries(target)
+    }
   }
 }
