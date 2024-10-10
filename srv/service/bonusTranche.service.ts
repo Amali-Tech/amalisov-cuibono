48695de1cd3b71dee2af98db2fe59dc80aef4324
Bateyjosue <josuebatey19@gmail.com>
2024-10-10 16:29:01 +0200

    fix: unregistrer the tranche participation service

--- srv/service/bonusTranche.service.ts
+++ srv/service/bonusTranche.service.ts
@@ -1,11 +1,10 @@
 import { Service } from "@sap/cds";
 import { createCombinedHandler } from "cds-routing-handlers";
 import { BonusTrancheHandler } from "../../src/handlers/bonusTranche.handler";
-import {TrancheParticipationHandler} from "../../src/handlers/trancheParticipation.handler";
 
 module.exports = (srv: Service) => {
   const combinedHandler = createCombinedHandler({
-    handler: [BonusTrancheHandler, TrancheParticipationHandler],
+    handler: [BonusTrancheHandler],
     middlewares: [],
   });
   combinedHandler(srv);
