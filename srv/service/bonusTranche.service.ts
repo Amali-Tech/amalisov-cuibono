8cc90fda97945ec739df1ffb368c8844ac69ce9f
Bateyjosue <josuebatey19@gmail.com>
2024-10-22 18:27:04 +0200

    feat: register the overruleParticipantAction

--- srv/service/bonusTranche.service.ts
+++ srv/service/bonusTranche.service.ts
@@ -2,10 +2,15 @@ import { Service } from "@sap/cds";
 import { createCombinedHandler } from "cds-routing-handlers";
 import { BonusTrancheHandler } from "../../src/handlers/bonusTranche.handler";
 import { UpdateBonusTranche } from "../../src/handlers/update-bonusTranche.action";
+import { OverruleParticipantAction } from "../../src/handlers/overrule-participant.action";
 
 module.exports = (srv: Service) => {
   const combinedHandler = createCombinedHandler({
-    handler: [BonusTrancheHandler, UpdateBonusTranche],
+    handler: [
+      BonusTrancheHandler,
+      UpdateBonusTranche,
+      OverruleParticipantAction
+    ],
     middlewares: [],
   });
   combinedHandler(srv);
