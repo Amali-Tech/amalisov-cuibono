bd7b1ac4a825a85669d3284e620a2b50521a5514
HenryRukundo3 <145010437+HenryRukundo3@users.noreply.github.com>
2024-10-23 12:33:20 +0200

    fix: onNavBack

--- app/amalisov.cuibono/webapp/controller/NotFound.controller.ts
+++ app/amalisov.cuibono/webapp/controller/NotFound.controller.ts
@@ -6,4 +6,7 @@ export default class NotFound extends BaseController {
      * Navigates back to the previous page in history if available,
      * otherwise navigates to the home route.
      */
+    public onLogoClick() {
+        this.getRouter().navTo("RouteMain")
+    }
 }
